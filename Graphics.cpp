b9dcea72fda48796e68a1a766c54b13080eb4793
chaKnox <cmknox51643@mymail.tstc.edu>
2017-05-09 17:14:06 -0500

    worked on graphics class

--- Graphics.cpp
+++ Graphics.cpp
@@ -2,11 +2,39 @@
 
 
 
+bool Graphics::Render()
+{
+	return false;
+}
+
 Graphics::Graphics()
 {
+
 }
 
 
 Graphics::~Graphics()
+{
+}
+
+bool Graphics::Initialized(int, int, HWND)
+{
+	return false;
+}
+
+void Graphics::Shutdown()
+{
+}
+
+bool Graphics::Frame()
+{
+	return false;
+}
+
+void Graphics::BeginScene(float, float, float, float)
+{
+}
+
+void Graphics::EndScene()
 {
 }
--- Graphics.h
+++ Graphics.h
@@ -1,10 +1,31 @@
 #pragma once
+#include <d3d9.h>
+#include <d3dx9.h>
+
 class Graphics
 {
 private:
+	bool m_vsync_enabled;
+	int m_viideoCardMemrory;
+	char m_videoCardDexcription[128];
+	LPDIRECT3DDEVICE9 m_Device;
+	D3DXMATRIX m_projectionMatrix;
+	D3DXMATRIX m_worldMatrix;
+	D3DXMATRIX m_orthoMatrix;
 
+	bool Render();
 public:
     Graphics();
     ~Graphics();
+	bool Initialized(int, int, HWND);
+	void Shutdown();
+	bool Frame();
+
+	void BeginScene(float, float, float, float);
+	void EndScene();
+	LPDIRECT3DDEVICE9 GetDevice() { return m_Device; }
+	void GetProjectionMatrix(D3DXMATRIX& projMat) { projMat = m_projectionMatrix; }
+	void GetWorldMatrix(D3DXMATRIX& worldMat) { worldMat = m_worldMatrix; }
+	void GetOrthoMatrix(D3DXMATRIX& orthoMat) { orthoMat = m_orthoMatrix; }
 };
 
